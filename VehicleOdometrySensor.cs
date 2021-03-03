69a48a09e105d34d8db81792c765eec5098192bb
eric.boise <eric.boise@lge.com>
2021-03-03 15:52:56 -0800

    add analysis for dist, speed violation and start stop position

--- VehicleOdometrySensor.cs
+++ VehicleOdometrySensor.cs
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020 LG Electronics, Inc.
+ * Copyright (c) 2020-2021 LG Electronics, Inc.
  *
  * This software contains code licensed as described in LICENSE.
  *
@@ -11,6 +11,10 @@ using Simulator.Utilities;
 using UnityEngine;
 using Simulator.Sensors.UI;
 using System.Collections.Generic;
+using Simulator.Map;
+using System;
+using System.Collections;
+using Simulator.Analysis;
 
 namespace Simulator.Sensors
 {
@@ -21,24 +25,42 @@ namespace Simulator.Sensors
         [Range(1f, 100f)]
         public float Frequency = 10.0f;
 
-        uint SendSequence;
-        float NextSend;
+        private uint SendSequence;
+        private float NextSend;
 
-        BridgeInstance Bridge;
-        Publisher<VehicleOdometryData> Publish;
+        private BridgeInstance Bridge;
+        private Publisher<VehicleOdometryData> Publish;
 
-        Rigidbody RigidBody;
-        IVehicleDynamics Dynamics;
-        VehicleActions Actions;
+        private Rigidbody RigidBody;
+        private IVehicleDynamics Dynamics;
+        private VehicleActions Actions;
+        private VehicleLane Lane;
+        private AgentController AgentController;
 
-        VehicleOdometryData msg;
-        
+        private VehicleOdometryData msg;
+
+        [AnalysisMeasurement(MeasurementType.Distance)]
+        private float Distance = 0f;
+        private Vector3 PrevPos = new Vector3(0f, 0f, 0f);
+
+        [AnalysisMeasurement(MeasurementType.RightHandPos)]
+        private Vector3 StartPosition;
+        [AnalysisMeasurement(MeasurementType.RightHandPos)]
+        private Vector3 EndPosition => transform.position;
+
+        private MapTrafficLane SpeedViolationLane;
+        private float SpeedViolationCount = 0f;
+        private float SpeedViolationMin = 0f;
+        private float SpeedViolationMax = 0f;
+
         public override SensorDistributionType DistributionType => SensorDistributionType.LowLoad;
 
         private void Awake()
         {
             RigidBody = GetComponentInParent<Rigidbody>();
             Dynamics = GetComponentInParent<IVehicleDynamics>();
+            AgentController = GetComponentInParent<AgentController>();
+            Lane = GetComponentInParent<VehicleLane>();
         }
 
         public override void OnBridgeSetup(BridgeInstance bridge)
@@ -50,17 +72,33 @@ namespace Simulator.Sensors
         public void Start()
         {
             NextSend = Time.time + 1.0f / Frequency;
+            StartPosition = transform.position;
         }
 
         public void Update()
         {
-            if (Time.time < NextSend)
-            {
-                return;
-            }
-            NextSend = Time.time + 1.0f / Frequency;
+            // distance analysis
+            Distance += Vector3.Distance(transform.position, PrevPos) / 1000;
+            PrevPos = transform.position;
 
+            // traffic speed violation
             float speed = RigidBody.velocity.magnitude;
+            if (speed > Lane?.CurrentMapLane?.speedLimit)
+            {
+                SpeedViolationLane = Lane.CurrentMapLane;
+                SpeedViolationCount += Time.fixedDeltaTime;
+                UpdateMinMax(speed, ref SpeedViolationMin, ref SpeedViolationMax);
+            }
+            else
+            {
+                if (SpeedViolationCount > 0 && SpeedViolationLane != null)
+                {
+                    SpeedViolationEvent(AgentController.GTID, SpeedViolationLane);
+                }
+                SpeedViolationCount = 0f;
+                SpeedViolationMax = 0f;
+                SpeedViolationLane = null;
+            }
 
             msg = new VehicleOdometryData()
             {
@@ -70,11 +108,29 @@ namespace Simulator.Sensors
                 SteeringAngleBack = 0f,
             };
 
+            if (Time.time < NextSend)
+            {
+                return;
+            }
+            NextSend = Time.time + 1.0f / Frequency;
+
             if (Bridge != null && Bridge.Status == Status.Connected)
             {
                 Publish(msg);
             }
+        }
 
+        private void UpdateMinMax(float value, ref float min, ref float max)
+        {
+            if (value < min)
+            {
+                min = value;
+            }
+
+            if (value > max)
+            {
+                max = value;
+            }
         }
 
         public override void OnVisualize(Visualizer visualizer)
@@ -88,9 +144,14 @@ namespace Simulator.Sensors
 
             var graphData = new Dictionary<string, object>()
             {
-                {"Speed", msg.Speed},
-                {"Steering Front", msg.SteeringAngleFront},
-                {"Steering Back", msg.SteeringAngleBack},
+                { "Speed", msg.Speed },
+                { "Steering Front", msg.SteeringAngleFront },
+                { "Steering Back", msg.SteeringAngleBack },
+                { "Start Position", StartPosition },
+                { "Distance", Distance },
+                { "CurrentSpeedLimit", SpeedViolationLane?.speedLimit },
+                { "SpeedViolationDuration", TimeSpan.FromSeconds(SpeedViolationCount).ToString() },
+                { "SpeedViolationMax", SpeedViolationMax },
             };
             visualizer.UpdateGraphValues(graphData);
         }
@@ -98,6 +159,22 @@ namespace Simulator.Sensors
         public override void OnVisualizeToggle(bool state)
         {
             //
+        }
+
+        private void SpeedViolationEvent(uint id, MapTrafficLane laneData)
+        {
+            Hashtable data = new Hashtable
+            {
+                { "Id", id },
+                { "Type", "SpeedViolation" },
+                { "Time", SimulatorManager.Instance.GetSessionElapsedTimeSpan().ToString() },
+                { "Location", transform.position },
+                { "SpeedLimit", laneData.speedLimit },
+                { "MaxSpeed", SpeedViolationMax },
+                { "Duration", TimeSpan.FromSeconds(SpeedViolationCount).ToString() },
+                { "Status", AnalysisManager.AnalysisStatusType.Failed },
+            };
+            SimulatorManager.Instance.AnalysisManager.AddEvent(data);
         }
     }
 }
